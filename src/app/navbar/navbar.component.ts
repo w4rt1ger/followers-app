cec477691941f5fca83837c49b3f79ca84ae2ea2
w4rt1ger <42971409+w4rt1ger@users.noreply.github.com>
2021-06-06 14:25:25 +0200

    Navbar auf Weiss umgeschalten

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
+  // tslint:disable-next-line: component-selector
   selector: 'navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  backgroundColor = environment.navBarBackgroundColor;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
